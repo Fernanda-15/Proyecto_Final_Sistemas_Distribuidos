94bd9d37b1eb72add95c9c2a5ec472536dd40a2e
Fernanda-15 <fernandaqp15@gmail.com>
2023-11-21 15:01:04 -0600

    prueba

--- src/components/BookIndex.vue
+++ src/components/BookIndex.vue
@@ -50,7 +50,7 @@
     },
     methods: {
       allBooks() {
-        fetch(this.url+'/.netlify/functions/bookFindAll',
+        fetch(this.url+'/netlify/functions/bookFindAll',
           { headers: {'Accept': 'application/json'}})
           .then((response) => response.json())
           .then((items) => {
