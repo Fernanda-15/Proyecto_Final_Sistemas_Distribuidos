80a5a81bc0e4676f057b3b547f1b3bdf1198fdaa
Fernanda-15 <fernandaqp15@gmail.com>
2023-11-22 15:54:15 -0600

    prueba backend

--- netlify/functions/movieInsert.js
+++ netlify/functions/movieInsert.js
@@ -1,30 +1,26 @@
-"use strict"
+"use strict";
 
 const redis = require('./redisDB');
 const headers = require('./headersCORS');
 
-function toJson(item, index, arr) {
-  arr[index] = JSON.parse(item);
-}
-
 exports.handler = async (event, context) => {
+    if (event.httpMethod === "OPTIONS") {
+        return { statusCode: 200, headers, body: "OK" };
+    }
 
-  if (event.httpMethod == "OPTIONS") {
-    return { statusCode: 200, headers, body: "OK" };
-  }
+    try {
+        redis.on("connect", function () {
+            console.log("You are now connected");
+        });
 
-  try {
-    
-    redis.on("connect", function() {
-      console.log("You are now connected");
-    });
-   
-   const data = JSON.parse(event.body);
+        const data = JSON.parse(event.body);
 
-   await redis.put(data.id,event.body);
-   return { statusCode: 200, headers, body: 'OK'};
-  } catch (error) {
-    console.log(error);
-    return { statusCode: 400, headers, body: JSON.stringify(error) };
-  }
+        await redis.put(data.id, event.body);
+        await redis.incr('movie_N');
+
+        return { statusCode: 200, headers, body: 'OK' };
+    } catch (error) {
+        console.log(error);
+        return { statusCode: 400, headers, body: JSON.stringify(error) };
+    }
 };
